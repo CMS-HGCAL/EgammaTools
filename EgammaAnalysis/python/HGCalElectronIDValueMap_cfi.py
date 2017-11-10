87e6e5ceeb6cf78c04c3004a7f6a973c9ac17e24
Florian Beaudette <Florian.Beaudette@cern.ch>
2017-11-10 16:56:31 +0100

    ../plugins/HGCalElectronIDValueMapProducer.cc

--- EgammaAnalysis/python/HGCalElectronIDValueMap_cfi.py
+++ EgammaAnalysis/python/HGCalElectronIDValueMap_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi import dEdX_weights as dEdX
 
 HGCalElectronIDValueMap = cms.EDProducer("HGCalElectronIDValueMapProducer",
-    electrons = cms.InputTag("ecalDrivenGsfElectronsFromMultiCl"),
+#    electrons = cms.InputTag("ecalDrivenGsfElectronsFromMultiCl"),
     pcaRadius = cms.double(3.),
     EERecHits = cms.InputTag('HGCalRecHit:HGCEERecHits'),
     FHRecHits = cms.InputTag('HGCalRecHit:HGCHEFRecHits'),
